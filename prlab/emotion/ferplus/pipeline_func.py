e467546c92541ad8dd39d0796c7fab5a21e27f51
hung <thanhhungqb@gmail.com>
2020-03-09 16:29:38 +0900

    pre_processing func for ferplus

---
+++ prlab/emotion/ferplus/pipeline_func.py
@@ -0,0 +1,19 @@
+"""
+Implement some pipeline func support `Pipeline Process template` in `prlab.fastai.pipeline.pipeline_control`
+"""
+from fastai.vision import *
+
+from prlab.emotion.emo_const import ferlabels
+
+
+def pre_processing(learn, **config):
+    """
+    implement some pre_processing for config before load data and train.
+    support `Pipeline Process template`
+    should be add before any data load step in the pipeline.
+    :param learn:
+    :param config:
+    :return:
+    """
+    config['label_names'] = np.array(ferlabels)
+    return learn, config
