8e6ca1717f1dac57195e635f96a15a8fcdae4f79
hung <thanhhungqb@gmail.com>
2020-03-11 17:00:27 +0900

    add rotate_img_size to fer+ to could be add to pipelines to train in many steps with different img_size

--- prlab/emotion/ferplus/pipeline_func.py
+++ prlab/emotion/ferplus/pipeline_func.py
@@ -16,3 +16,22 @@ def pre_processing(**config):
     """
     config['label_names'] = np.array(ferlabels)
     return config
+
+
+def rotate_img_size(**config):
+    """
+    `Pipeline Process template`
+    TO ratate img_size (input image size) in config in order
+    :param config: img_size, img_size_rotate
+    :return: config with update img_size
+    """
+    rotate_size = config['img_size_rotate']
+    cur_pos = [i for i in range(len(rotate_size)) if rotate_size[i] == config['img_size']]
+    if len(cur_pos) == 1:
+        cur_pos = cur_pos[0]
+        next_pos = cur_pos + 1 if cur_pos + 1 < len(rotate_size) else 0
+        config['img_size'] = rotate_size[next_pos]
+    else:
+        "Do nothing, error here: 0 -> size is not in list, >1: multi same value"
+
+    return config
